d0b71a0c7c265198aa89ad82926cc94a1faf82c9
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2025-07-10 11:32:05 +0700

    adding permission module

---
+++ src/modules/permission/model.rs
@@ -0,0 +1,35 @@
+use async_trait::async_trait;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, Error as SqlxError, query_scalar};
+use uuid::Uuid;
+use crate::db::DBClient;
+
+#[derive(Debug, Deserialize, Serialize, FromRow, Clone)]
+pub struct Permission {
+    pub id: Uuid,
+    pub name: String,
+    pub description: String,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
+}
+
+#[async_trait]
+pub trait PermissionRepository {
+    async fn get_permission_by_role(&self, role_id: &Uuid) -> Result<Vec<String>, SqlxError>;
+}
+
+#[async_trait]
+impl PermissionRepository for DBClient {
+    async fn get_permission_by_role(&self, role_id: &Uuid) -> Result<Vec<String>, SqlxError> {
+        let permissions = query_scalar!(
+                r#"
+                    SELECT p.name FROM permissions AS p
+                    JOIN role_permissions AS rp ON rp.permission_id = p.id
+                    WHERE rp.role_id = $1;
+                "#,
+                role_id
+            ).fetch_all(&self.pool).await?;
+        Ok(permissions)
+    }
+}
